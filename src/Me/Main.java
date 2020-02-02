399c092075b42280eb443af6f173541595cccd2d
Alex Vasiuk <51331805+oleksiivas@users.noreply.github.com>
2020-02-01 20:13:48 -0700

    Update Main.java

--- src/Me/Main.java
+++ src/Me/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args){
         //did this work
         System.out.println("Hello my name is Joshua");
+        //Yes, it worked 
+        System.out.println("Hello, Joshua, my name is Alex!");
 
     }
 }
